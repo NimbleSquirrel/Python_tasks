efafd4ac34b95711bf3dcfc409cdd5431892e14c
NimbleSquirrel <milapetrovsk@gmail.com>
2022-09-18 17:39:02 +0300

    functions.py
    processing various values from the user and checking if the condition is met

--- 2.8.6 .py
+++ 2.8.6 .py
@@ -1,31 +1,5 @@
-from typing import Tuple
-
-def parse_args() -> Tuple[list[int], list[int] | None]:
-    """
-    Get input values from user, add them into rps_values list
-    :return: new_values, delimiters
-    """
-    new_value = None  # keeps new values to define condition
-
-    new_values: list[int] = []
-
-    delimiters = None  # keeps values as list
-
-    # check conditions
-    while new_value != ' ' and new_value != '[':
-        new_value = input("enter value:")
-        if new_value == ' ':  # break input
-            break
-        if ';' in new_value:  # a;b;c
-            new_values += list(map(int, new_value.split(';')))
-            continue
-        if '[' in new_value:  # [a, b]
-            delimiters = list(map(int, new_value.strip('][').split(', ')))
-            new_values += delimiters
-            break
-        new_values.append(int(new_value))
-
-    return (new_values, delimiters)
+def cast_to_int(strings: list[str]) -> list[int]:
+    return list(map(int, [symbol for symbol in strings if symbol.isdigit()]))
 
 def get_filtered_data(data: list[int], delimiters: list[int]):
     """
@@ -33,14 +7,14 @@ def get_filtered_data(data: list[int], delimiters: list[int]):
     [left slice, right slice]
     :param data: rps_values fetched to int and sorted from smallest to largest
     :param delimiters: list with 2 values for slice
-    :return: data_left_slice + data_right_slice
+    :return: data
     """
     first_delimiter, second_delimiter = sorted(delimiters)
     # slice list by inserted values left and right part
-    data_left_slice = data[:data.index(first_delimiter) + 1]
-    data_right_slice = data[data.index(second_delimiter):]
+    data = data[slice(first_delimiter, second_delimiter + 1)]
     # unite values from left and right part
-    return data_left_slice + data_right_slice
+    return data
+
 
 def calc_average(data: list[int]) -> int:
     """
@@ -57,6 +31,7 @@ def calc_median(data: list[int]) -> int | float:
     :param data: rps_values
     :return: median
     """
+    data = sorted(data)
     quotient, remainder = divmod(len(data), 2)
     return data[quotient] if remainder else sum(data[quotient - 1: quotient + 1]) / 2
 
@@ -69,6 +44,13 @@ def calc_average_and_mean_diff(average: int | float, median: int | float) -> flo
     """
     return (abs(average - median) / average) * 100
 
+def determine_load_type():
+    if calc_average_and_mean_diff(average, median) <= 25:
+        return print("Нагрузка стабильна")
+    elif average > median:
+        return print("Происходят скачки")
+    else:
+        return print("Происходят снижения")
 
 if __name__ == "__main__":
     rps_values = [5081, '17184', 10968, 9666, '9102', 12321, '10617', 11633, 5035, 9554, '10424', 9378, '8577',
@@ -77,30 +59,54 @@ if __name__ == "__main__":
                   '13657', 15817, '16187', '6381', 8409, '5177', 17357, '10814', 6679, 12241, '6556', 12913, 16454,
                   '17589', 5292, '13639', '7335', '11531', '14346', 7493, 15850, '12791', 11288]
 
-    duplicate_values = {}
-
     # convert all str values in list to int
     rps_values = list(map(int, rps_values))
 
-    new_values, delimiters = parse_args()
-    rps_values += new_values
-    rps_values = sorted(rps_values)
+    new_values: list[int] = []
+
+    delimiters = None
+
+    while True:
+        new_value = input("enter values:")
+        if new_value.strip() == '':  # break input
+            break
+        if ';' in new_value:  # a;b;c
+            new_values += cast_to_int(new_value.split(';'))
+            continue
+        if '[' in new_value:  # [a, b] or [a,b]
+            delimiters = cast_to_int(new_value.strip('][').split(','))
+            if len(delimiters) != 2:
+                delimiters = None
+                break
+        if new_value.isdigit():
+            new_values.append(int(new_value))
 
     if delimiters is not None:
         rps_values = get_filtered_data(rps_values, delimiters)
+        rps_values += new_values
+        print(rps_values)
 
-    average = calc_average(rps_values)
-    median = calc_median(rps_values)
+        average = calc_average(rps_values)
+        median = calc_median(rps_values)
 
-    # determine the type of load
-    if calc_average_and_mean_diff(average, median) <= 25:
-        print("Нагрузка стабильна")
-    elif average > median:
-        print("Происходят скачки")
-    else:
-        print("Происходят снижения")
+        determine_load_type()
 
-    for value in set(rps_values):
-        duplicate_values[value] = rps_values.count(value)
-    print("Частоты встреченных значений: \n")
-    print(duplicate_values)
+        # подсчет частоты встречающихся значений
+        frequency = {}
+        for value in rps_values:
+            frequency[value] = frequency.get(value, 0) + 1
+        print(frequency)
+    elif new_values:
+        rps_values += new_values
+        print(rps_values)
+
+        average = calc_average(rps_values)
+        median = calc_median(rps_values)
+
+        determine_load_type()
+
+        # подсчет частоты встречающихся значений
+        frequency = {}
+        for value in rps_values:
+            frequency[value] = frequency.get(value, 0) + 1
+        print(frequency)
